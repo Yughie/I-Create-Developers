80b5a171999b257395e362530c1f22b790298ae2
Yughie <yughiep@gmail.com>
2022-06-29 14:27:42 +0800

    Added more media query

--- ACTIVITY2_RESPONSIVE/index.js
+++ ACTIVITY2_RESPONSIVE/index.js
@@ -1,9 +1,11 @@
 var navbar = document.getElementById("navigation_bar");
-
+var hiddenlogo = document.getElementById("hiddenLogo");
 function showMenu(){
     navbar.style.right = "0%";
+    hiddenlogo.style.display = "none"
 }
 
 function hideMenu(){
     navbar.style.right ="-50%"
+    hiddenlogo.style.display = "block"
 }
